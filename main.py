eb2b2906b150cf3cd282e62b32d67b1c9684b3b9
LeeSnake <311210@whut.edu.cn>
2022-10-09 21:24:12 +0800

    final

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import jieba
+import jieba.analyse
 from wordcloud import WordCloud
 
 
 def fenci(tempstr):
-    wordlist = jieba.cut(tempstr)
+    wordlist = jieba.analyse.extract_tags(tempstr)
     result = ' '.join(wordlist)
     return result
 
@@ -15,17 +15,17 @@ if __name__ == '__main__':
     string = ''
     for i in range(len(comment)):
         string += fenci(comment[i])
-    stopwords = {'豆瓣', '说', '本', '书', '本书', '一本', '想', '写', '阅读', '建议', '看', 'comment', '都', '不', '没有', '不是'}
-    content = [line.strip() for line in open('hit_stopwords.txt', 'r', encoding='utf-8').readlines()]
+    stopwords = {'豆瓣', '书', '本书', '一本', '想', '说', '写', '读', '读完', '读过', '看', 'comment'}
+    content = [line.strip() for line in open('cn_stopwords.txt', 'r', encoding='utf-8').readlines()]
     stopwords.update(content)
     w = WordCloud(font_path="msyh.ttc",       # 微软雅黑
                   width=1600,
                   height=800,
                   background_color="white",   # 白色背景
-                  max_font_size=150,          # 最大字体
-                  max_words=1000,             # 最大词数
+                  max_font_size=300,          # 最大字体
+                  max_words=100,             # 最大词数
                   stopwords=stopwords)
     w.generate(string)
     image = w.to_image()
-    w.to_file('ciyun.png')
+    w.to_file('wordcloud.png')
     image.show()
